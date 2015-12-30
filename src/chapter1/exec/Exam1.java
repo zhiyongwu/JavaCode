312bb03828adbcec7d013f6f645eea9a98bc9c00
wzy <wzy@rsvptech.cn>
2015-12-30 10:31:52 +0800

    First Update

---
+++ src/chapter1/exec/Exam1.java
@@ -0,0 +1,11 @@
+package chapter1.exec;
+
+/**
+ * Created by Administrator on 2015/12/30.
+ */
+public class Exam1 {
+    public static void main(String[] args) {
+        System.out.println("morning");
+        System.out.println("afternoon");
+    }
+}
