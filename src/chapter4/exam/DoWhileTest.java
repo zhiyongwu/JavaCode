e066f78a581fffcb38b2e62ca5838a7b929eb7ae
zhiyongwu <zhiyongwu@live.cn>
2016-01-13 13:52:19 +0800

    chapter4

---
+++ src/chapter4/exam/DoWhileTest.java
@@ -0,0 +1,20 @@
+package chapter4.exam;
+
+import java.util.Scanner;
+
+/**
+ * Created by Administrator on 2016/1/13.
+ */
+public class DoWhileTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = -1;
+        int sum = 0;
+        System.out.println("Enter a number (if 0 exits)");
+        do {
+            input = scanner.nextInt();
+            sum += input;
+        }while (input != 0);
+        System.out.println(sum);
+    }
+}
---
+++ src/chapter4/exam/MonteCarloSimulation.java
@@ -0,0 +1,24 @@
+package chapter4.exam;
+
+/**
+ * Created by Administrator on 2016/1/13.
+ */
+public class MonteCarloSimulation {
+    public static void main(String[] args) {
+        final int NUMBER_OF_TRIALS = 1000000000;
+        int numberOfHits = 0;
+
+        for (int i = 0; i < NUMBER_OF_TRIALS; i++) {
+            double x = Math.random()*2.0 - 1;
+            double y = Math.random()*2.0 - 1;
+
+            if(Math.sqrt(Math.pow(x,2) + Math.pow(y,2)) < 1){
+                numberOfHits++;
+            }
+        }
+
+
+        double pi = 4.0 * numberOfHits / NUMBER_OF_TRIALS;
+        System.out.println(pi);
+    }
+}
