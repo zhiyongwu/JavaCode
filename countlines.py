3696ffa456db52e0dc6b15f2704d86e25c60158e
zhiyongwu <zhiyongwu@live.cn>
2016-01-29 08:39:06 +0800

    更新统计代码行数

--- countlines.py
+++ countlines.py
@@ -1,21 +1,17 @@
+#-*-encoding:utf-8-*-
 import os
 import os.path
 
 rootdir = "."
 count = 0
 for parent, dirnames, filenames in os.walk(rootdir):
-    # for dirname in dirnames:
-    #     print(dirname)
-    for filename in filenames:
-    	filename = parent+ '/' + filename
-    	if 'java' in filename:
-        	file = open(filename,'r')
-       		for line in file.readlines():
-       			if '//' not in line and '/*' not in line and '*/' not in line and line != chr(10) and line != '\n' and line != '':
-        			count += 1
+	for filename in filenames:
+		filename = parent+ '/' + filename
+		if filename.split('.')[2] == 'java':
+			file = open(filename,'r',encoding='utf8',errors='ignore')
+			for line in file.readlines():
+				if '//' not in line and '/*' not in line and '*/' not in line and line != chr(10) and line != '\n' and line != '':
+					count += 1
 print(count)
 
 
-# file = open('./src/Java/chapter1/exam/Welcome.java','r')
-# for line in file.readlines():
-# 	print(line)
